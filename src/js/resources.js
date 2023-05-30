1e15d621ced0254508d149c50771a13e33f98820
lindsey <lindseytaak123@gmail.com>
2023-05-30 11:42:06 +0200

    oefenen

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,11 +1,13 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 import backgroundImage from '../images/bg.png'
+import backGroundImage from '../images/bg-grond.png'
 import mainCharacterImage from '../images/main-character.png'
 
 const Resources = {
     MainCharacter: new ImageSource(mainCharacterImage),
-    background: new ImageSource(backgroundImage)
+    background: new ImageSource(backgroundImage),
+    backGround: new ImageSource(backGroundImage)
 }
 const ResourceLoader = new Loader([Resources.MainCharacter , Resources.background])
 
